e964d58243599343e896957617159761e946d123
Gabrielle <gabrielle.castro@ymonetize.com>
2023-07-23 03:18:18 -0300

    at

--- src/app/Devices/page.tsx
+++ src/app/Devices/page.tsx
@@ -1,59 +1,85 @@
 "use client"
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
 import useSWR from "swr"
-import { Api } from "../../services/api"
+import { Api, Api_Gazin } from "../../services/api"
 
 export default function Devices() {
+  const [filial, setFilial] = useState('10002')
 
   function useDevices() {
     let address = `devices`
 
     const fetcher = async (address: string) => await Api.get(address).then((res) => res.data)
-    const { data, mutate } = useSWR(address, fetcher, { refreshInterval: 3000 })
+    const { data, isLoading, mutate } = useSWR(address, fetcher, { refreshInterval: 1000 })
 
     return {
       devices_: data,
+      isLoading,
       mutate
     }
   }
 
-  const { devices_ } = useDevices()
+  const { devices_, isLoading } = useDevices()
+
+  const valueApi = devices_?.map((item: { device: any }) => (item.device))
+  const valueFilial = devices_?.map((item: { branch: any }) => (item.branch))
+  const valueColor = devices_?.map((item: { color: any }) => (item.color))
+
+  function useDevicesGazin() {
+    let address = `celulares?idfilial=${filial}&token=Gazin-tech%C3%87$2y$10$85Udhj9L4Pa9XULE5RxyTu0Yv5G0POBiS7u2Yb693P9o6Ctege7cq%C3%87Gazin-tech`
+
+    const fetcher = async (address: string) => await Api_Gazin.get(address).then((res) => res.data)
+    const { data, isLoading, mutate } = useSWR(address, fetcher, { refreshInterval: 1000 })
+
+    return {
+      devices_gazin: data,
+      isLoading,
+      mutate
+    }
+  }
+
+  const { devices_gazin } = useDevicesGazin()
 
   return (
     <main className={styles.main}>
       <div className={styles.content}>
         <h1>APARELHOS CONECTADOS</h1>
         <div className={styles.line} />
+        {isLoading == false ? (
+          <>
+            <text>TOTAL: 0</text>
 
-        <text>TOTAL: {devices_?.length}</text>
+            <div className={styles.content_grid}>
+              {devices_gazin?.map((item: { produto: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; idproduto: React.Key | null | undefined; cor: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; precopartida: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; precoaprazo: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.PromiseLikeOfReactNode | null | undefined }) => {
+                if (valueApi == item.produto && valueFilial == filial && valueColor == item.cor) return (
+                  <div key={item.idproduto} className={styles.grid}>
+                    <div>
+                      <h2>{item.produto} - {item.cor}</h2>
+                    </div>
 
-        <div className={styles.content_grid}>
-          {devices_?.map((item: { id: React.Key | null | undefined; device: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; price: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; price_card: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; price_desk: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; branch: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined }) => (
-            <div key={item.id} className={styles.grid}>
-              <div>
-                <h2>{item.device}</h2>
-              </div>
+                    <div>
+                      <h6>R${item.precopartida}</h6>
+                      <p>(a vista)</p>
+                    </div>
 
-              <div>
-                <h6>{item.price}</h6>
-                <p>(a vista)</p>
-              </div>
+                    <div>
+                      <h6>R${item.precoaprazo}</h6>
+                      <p>(cartão)</p>
+                    </div>
 
-              <div>
-                <h6>{item.price_card}</h6>
-                <p>(cartão)</p>
-              </div>
-
-              <div>
-                <h6>{item.price_desk}</h6>
-                <p>(carne)</p>
-              </div>
-              <h4>{item.branch}</h4>
+                    <div>
+                      <h6>R${item.precoaprazo}</h6>
+                      <p>(carne)</p>
+                    </div>
+                    <h4>{filial}</h4>
+                  </div>
+                )
+              })}
             </div>
-          ))}
-        </div>
+          </>
+        ) : (null)}
       </div>
     </main>
   )
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,10 @@ const Api = axios.create({
     baseURL: 'http://207.244.238.126:8888'
 })
 
+const Api_Gazin = axios.create({
+    baseURL: 'https://api.gazin.com.br/varejo/relatorio/promocoes'
+})
+
+export { Api_Gazin }
+
 export { Api }

820d33c20af0c4ff818c9fbf21f66628852995be
Gabrielle <gabrielle.castro@ymonetize.com>
2023-08-19 00:52:33 -0300

    at1

--- src/app/Devices/page.tsx
+++ src/app/Devices/page.tsx
@@ -1,15 +1,16 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
 import useSWR from "swr"
-import { Api, Api_Gazin } from "../../services/api"
+import { Api } from "../../services/api"
 import { ThreeDots } from 'react-loader-spinner'
 import { DebounceInput } from 'react-debounce-input'
 import { FiSearch } from "react-icons/fi"
 
 export default function Devices() {
   const [search, setSearch] = useState('10002')
+  const [length, setLength] = useState()
 
   function useDevices() {
     let address = `devices`
@@ -18,91 +19,95 @@ export default function Devices() {
     const { data, isLoading, mutate } = useSWR(address, fetcher, { refreshInterval: 1000 })
 
     return {
-      devices_: data,
+      devices: data,
       isLoading,
       mutate
     }
   }
 
-  const { devices_ } = useDevices()
+  const { devices, isLoading } = useDevices()
 
-  function useDevicesGazin() {
-    let address = `celulares?idfilial=${search}&token=Gazin-tech%C3%87$2y$10$85Udhj9L4Pa9XULE5RxyTu0Yv5G0POBiS7u2Yb693P9o6Ctege7cq%C3%87Gazin-tech`
+  const count = () => {
+    const devices_app = devices?.map(item => item.branch)
+    var quantidade = 0
 
-    const fetcher = async (address: string) => await Api_Gazin.get(address).then((res) => res.data)
-    const { data, isLoading, mutate } = useSWR(address, fetcher, { refreshInterval: 1000 })
-
-    return {
-      devices_gazin: data,
-      isLoading,
-      mutate
+    for (var i = 0; i < devices?.length; i++) {
+      if (devices_app[i] == search) {
+        quantidade++
+      }
     }
+
+    setLength(quantidade)
   }
 
-  const { devices_gazin } = useDevicesGazin()
-
-  function mask(input: string): string {
-    input = parseFloat(input).toFixed(2)
-    input = input.toString().replace('.', ',')
-    const valor = input.replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-
-    return 'R$ ' + valor
-  }
+  useEffect(() => {
+    count()
+  }, [isLoading, search])
 
   return (
     <main className={styles.main}>
       <div className={styles.content}>
         <h1>APARELHOS CONECTADOS</h1>
         <div className={styles.line} />
-        <div className={styles.head}>
-          <text>TOTAL: 5</text>
+        {isLoading == false ? (
+          <>
+            <div className={styles.head}>
+              <text>TOTAL: {length}</text>
 
-          <div className={styles.boxInput}>
-            <div className={styles.buttonSearch} title="pesquisar">
-              <FiSearch className={styles.iconSearch} size={17} />
+              <div className={styles.boxInput}>
+                <div className={styles.buttonSearch} title="pesquisar">
+                  <FiSearch className={styles.iconSearch} size={17} />
+                </div>
+
+                <DebounceInput
+                  debounceTimeout={500}
+                  placeholder={search}
+                  onChange={(event) => setSearch(event.target.value)}
+                />
+              </div>
             </div>
 
-            <DebounceInput
-              debounceTimeout={500}
-              placeholder={search}
-              onChange={(event) => setSearch(event.target.value)}
+            <div className={styles.content_grid}>
+              {devices?.map(item => {
+                if (item.branch === search)
+                  return (
+                    <div key={item.id} className={styles.grid}>
+                      <div>
+                        <h2>{item.device} {item.color}</h2>
+                      </div>
+
+                      <div>
+                        <h6>R$0.000,00</h6>
+                        <p>(a vista)</p>
+                      </div>
+
+                      <div>
+                        <h6>R$0.000,00</h6>
+                        <p>(cartão)</p>
+                      </div>
+
+                      <div>
+                        <h6>R$0.000,00</h6>
+                        <p>(carne)</p>
+                      </div>
+                      <h4>{item.branch}</h4>
+                    </div>
+                  )
+              })}
+            </div>
+          </>
+        ) : (
+          <div className={styles.loader}>
+            <ThreeDots
+              height="60"
+              width="60"
+              radius="9"
+              color='#180c72'
+              ariaLabel='three-dots-loading'
             />
           </div>
-        </div>
-
-        <div className={styles.content_grid}>
-          <div className={styles.grid}>
-            <div>
-              <h2>nome do produto</h2>
-            </div>
-
-            <div>
-              <h6>R$0.000,00</h6>
-              <p>(a vista)</p>
-            </div>
-
-            <div>
-              <h6>R$0.000,00</h6>
-              <p>(cartão)</p>
-            </div>
-
-            <div>
-              <h6>R$0.000,00</h6>
-              <p>(carne)</p>
-            </div>
-            <h4>10002</h4>
-          </div>
-        </div>
+        )}
       </div>
     </main>
   )
 }
- /*<div style={{ marginTop: 20 }}>
-    <ThreeDots
-      height="60"
-      width="60"
-      radius="9"
-      color='#180c72'
-      ariaLabel='three-dots-loading'
-    />
-  </div>*/

b3c45fe8f04b5ec836ea7791da2f1b8295ab55d1
Gabrielle De Castro Santos Catarino <catarinogabrielle@gmail.com>
2024-05-10 22:28:32 -0300

    at1

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const Api = axios.create({
     baseURL: 'http://207.244.238.126:8888',
-    timeout: 7000,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
